978e9f89b54bb3d6fe5b79336fbc1a0765eba723
Jessica Grzanna <jgrzanna@me.com>
2018-01-29 20:55:43 +0100

    add test and implementation for dynamic index page

--- test/integration/display_items_test.rb
+++ test/integration/display_items_test.rb
@@ -1,15 +1,23 @@
 require 'test_helper'
 
 class DisplayItemsTest < Capybara::Rails::TestCase
-  def setup
-    Item.create!(title: 'T-Shirt', price: 19.99, currency: 'EUR')
-  end
 
   test 'items index shows title price and currency' do
+    Item.create!(title: 'T-Shirt', price: 19.99, currency: 'EUR')
+
     visit items_path
 
     assert page.has_content?('T-Shirt')
-    assert page.has_content?('19,99')
-    assert page.has_content?('EUR')
+    assert page.has_content?('19,99 EUR')
+  end
+
+  test 'items index shows another title price and currency' do
+    Item.create!(title: 'Shoes', price: 59.99, currency: 'EUR')
+
+    visit items_path
+
+    assert page.has_content?('Shoes')
+    assert page.has_content?('59,99 EUR')
+    assert_not page.has_content?('T-Shirt')
   end
 end
